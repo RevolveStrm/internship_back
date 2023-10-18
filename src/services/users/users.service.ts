12dead8e939f8f80f8d9f07d41e92a0eeb926154
RevolveStrm <revolvebattlerage@gmail.com>
2023-10-18 21:26:29 +0300

    first commit

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,79 +1,60 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private readonly lectorRepository: Repository<User>,
+    private readonly repository: Repository<User>,
   ) {}
 
-  async create(createLectorDto: CreateUserDto): Promise<User> {
-    const lector = await this.lectorRepository.findOne({
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user: User = await this.repository.findOne({
       where: {
-        email: createLectorDto.email,
+        email: createUserDto.email,
       },
     });
 
     const salt = await bcrypt.genSalt();
 
-    createLectorDto.password = await bcrypt.hash(createLectorDto.password, salt);
+    createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
 
-    if (lector) {
+    if (user) {
       throw new BadRequestException('Email already exist');
     }
 
-    return this.lectorRepository.save(createLectorDto);
+    return this.repository.save(createUserDto);
   }
 
   async findOne(id: string): Promise<User> {
-    const lector = this.lectorRepository.findOne({
+    const user = this.repository.findOne({
       where: {
         id,
       },
     });
 
-    if (!lector) {
+    if (!user) {
       throw new NotFoundException('Lector not found');
     }
 
-    return lector;
+    return user;
   }
 
   async findByEmail(email: string): Promise<User> {
-    const lector = this.lectorRepository.findOne({
+    const user: Promise<User> = this.repository.findOne({
       where: {
         email,
       },
     });
 
-    if (!lector) {
-      throw new NotFoundException('Lector not found');
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
 
-    return lector;
-  }
-
-  async update(id: string, updateLectorDto: UpdateUserDto): Promise<UpdateResult> {
-    await this.findOne(id);
-
-    if (updateLectorDto.password) {
-      const salt = await bcrypt.genSalt();
-
-      updateLectorDto.password = await bcrypt.hash(updateLectorDto.password, salt);
-    }
-
-    return await this.lectorRepository.update(id, updateLectorDto);
-  }
-
-  async remove(id: string): Promise<DeleteResult> {
-    await this.findOne(id);
-
-    return await this.lectorRepository.delete(id);
+    return user;
   }
 }
